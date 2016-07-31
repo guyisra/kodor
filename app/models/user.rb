619366cf1dad018b19968b9114656ab38918d820
Guy Israeli <guy.israeli@klarna.com>
2016-07-31 19:02:54 +0300

    user sscope

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,21 @@
 class User < ActiveRecord::Base
 
-  class << self
-    def from_omniauth(auth_hash)
-      raise '401' if !auth_hash['info'].email.end_with? '@klarna.com'
+ scope :casual_users, ->{ where(provider: "Klarna")}
 
-      user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
-      user.name = auth_hash['info']['name']
-      user.location = auth_hash['info']['location']
-      user.image_url = auth_hash['info']['image']
-      # user.url = auth_hash['info']['urls'][user.provider.capitalize]
-      user.save!
-      user
-    end
+  def self.from_omniauth(auth_hash)
+    raise '401' if !auth_hash['info'].email.end_with? '@klarna.com'
 
-    def from_uid(uid)
-      User.find_by(uid: uid)
-    end
+    user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
+    user.name = auth_hash['info']['name']
+    user.location = auth_hash['info']['location']
+    user.image_url = auth_hash['info']['image']
+    # user.url = auth_hash['info']['urls'][user.provider.capitalize]
+    user.save!
+    user
+  end
+
+  def self.from_uid(uid)
+    User.find_by(uid: uid)
   end
 
   def can_open?
