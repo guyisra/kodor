396506d4532e0251263b358b744c3c5036645b82
Lev Kazakov <lev.kazakov@klarna.com>
2016-07-31 17:08:28 +0300

    authorize only @klarna users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     begin
       @user = User.from_omniauth(request.env['omniauth.auth'])
       session[:user_id] = @user.id
-      flash[:success] = "#{request.env['omniauth.auth']}.to_y"
+      flash[:success] = "Welcome, #{@user.name}!"
     rescue
       flash[:warning] = "There was an error while trying to authenticate you..."
     end
