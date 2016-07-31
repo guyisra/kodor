4f33e55f4f53769df5550abccaed6ee115ecb11b
Guy Israeli <guy.israeli@klarna.com>
2016-07-31 15:43:38 +0300

    set open sesame phrase

--- app/controllers/door_controller.rb
+++ app/controllers/door_controller.rb
@@ -3,7 +3,7 @@ class DoorController < ApplicationController
   def open
     return head :unauthorized unless current_user
     redis_client = Redis.new
-    redis_client.publish(ENV['CHANNEL'], { msg: 'Open door' }.to_json)
+    redis_client.publish(ENV['CHANNEL'], ENV['OPEN_SESAME'] || "meow")
 
     head :ok
   end
