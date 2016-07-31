9790c5733f1b3abe0b3707c47e98fe8d64ff742e
Tomer Levi <tomer.levi@klarna.com>
2016-07-31 17:16:14 +0300

    add db migration and stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,19 @@ class PagesController < ApplicationController
   end
 
   def create
+    start_date = date_for(params[:start_date])
+    end_date = date_for(params[:end_date]).end_of_day
+    return head :bad_request if start_date < DateTime.now || end_date < start_date
+
     uid = SecureRandom.uuid
-    User.create(uid: uid, name: params[:name], provider: 'Klarna')
+    User.create({ uid: uid, name: params[:name], provider: 'Klarna' }.merge(params.slice(:name, :start_date, :end_date)))
 
     render json: { url: pages_url(uid) }
+  end
+
+  private
+
+  def date_for(string)
+    DateTime.strptime(string,"%Y-%m-%d")
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160731095516) do
+ActiveRecord::Schema.define(version: 20160731140539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20160731095516) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "start_date"
+    t.datetime "end_date"
   end
 
   add_index "users", ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true, using: :btree
